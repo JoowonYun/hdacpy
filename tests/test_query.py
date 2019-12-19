da64dd9b26de3f37d8b8e4d495008888d0f57017
Bryan <psy2848048@gmail.com>
2019-12-20 07:11:14 +0900

    Modified test of balance querying

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,15 +1,17 @@
 import json
 from unittest.mock import Mock
+import pytest
 
 from hdacpy.transaction import Transaction
 
+@pytest.mark.skip(reason="only works if RESTful server runs in local")
 def test_balance():
     tx = Transaction(
         host="http://localhost:1317",
         privkey="26d167d549a4b2b66f766b0d3f2bdbe1cd92708818c338ff453abde316a2bd59",
         account_num=11335,
         sequence=0,
-        gas=37000,
+        gas_price=37000,
         chain_id="friday-devtest",
     )
     resp = tx.balance(address="friday15evpva2u57vv6l5czehyk69s0wnq9hrkqulwfz")
